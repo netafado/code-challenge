8903d5cb5818097445ad1e0ccfb5246ed85b758d
netafado <isaias.fran@gmail.com>
2022-04-30 18:17:50 -0300

    chore: add  editorconfig

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import {FC} from "react"
+
+type HomeProps = {
+  title: string
+}
+
+const Home:FC<HomeProps> = ({ title = "Addi" }) => {
+  return (
+    <div>
+      {title}
+    </div>
+  )
+}
+
+export default Home
