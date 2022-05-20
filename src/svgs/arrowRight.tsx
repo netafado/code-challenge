5f0bd0af68f142ee18afe0a55f5bf23aa62cdc99
isaias_santos88 <isaias.santos@zennioptical.com>
2022-05-19 22:49:50 -0300

    feature: add svgs

---
+++ src/svgs/arrowRight.tsx
@@ -0,0 +1,20 @@
+// eslint-disable-next-line react/display-name
+const ArrowRight = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="7"
+    height="13"
+    viewBox="0 0 7 13"
+  >
+    <polyline
+      fill="none"
+      stroke="#000"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      points=".5 12.5 6.5 6.5 .5 .5"
+      opacity=".5"
+    />
+  </svg>
+)
+
+export default ArrowRight
